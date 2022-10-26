f5314f6f46947f135c01fc789b44975eedc5460d
Anderson Sales <andersonsales.mg@gmail.com>
2022-10-26 15:20:47 -0300

    project base created

---
+++ BrowserApp/BrowserApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  BrowserApp
+//
+//  Created by Anderson Sales on 26/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
