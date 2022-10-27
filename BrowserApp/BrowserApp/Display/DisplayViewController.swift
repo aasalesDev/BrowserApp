c209af84b88efc71b18ca57f237cfec246dd68e8
Anderson Sales <andersonsales.mg@gmail.com>
2022-10-27 14:04:11 -0300

    added UIAlert

--- BrowserApp/BrowserApp/Display/DisplayViewController.swift
+++ BrowserApp/BrowserApp/Display/DisplayViewController.swift
@@ -17,11 +17,11 @@ class DisplayViewController: UIViewController  {
         webView = WKWebView()
         webView.navigationDelegate = self
         view = webView
-        
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         let startUrl = "https://www."
         let endUrl = ".com"
         let url = URL(string: "\(startUrl)\(company!)\(endUrl)")!
@@ -29,8 +29,26 @@ class DisplayViewController: UIViewController  {
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
+    
+    @objc func openTapped(){
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "google.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction){
+        guard let actionTitle = action.title else { return }
+        guard let url = URL(string: "https://\(actionTitle)") else { return }
+        webView.load(URLRequest(url: url))
+    }
 }
 
 extension DisplayViewController: WKNavigationDelegate{
-    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 }
--- BrowserApp/BrowserApp/ViewController.swift
+++ BrowserApp/BrowserApp/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     @IBAction func accessButtonTapped(_ sender: UIButton) {
